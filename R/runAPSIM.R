5e000e77f7a353818f8ba568df9731e0a222be7f
stanfill <bryan.stanfill@csiro.au>
2014-03-14 15:37:58 +1000

    be able to read several out files at once

--- R/runAPSIM.R
+++ R/runAPSIM.R
@@ -13,5 +13,19 @@ apsimr<-function(wd){
   apsimEXE<-" \"C:/Program Files (x86)/Apsim75-r3008/Model/Apsim.exe\" "
   sim_file_name<- " *.apsim"
   system(paste(apsimEXE, sim_file_name, sep = " "), show.output.on.console = FALSE)
-  out_file<-read.table(paste("Continuous","Wheat.out",sep=" "),header=TRUE)
+  flist<-list.files()
+  
+  fileNames<-grep(".out",flist)
+  nfiles<-length(fileNames)
+  
+  outNames<-flist[fileNames]
+  
+  out_file<-vector("list",nfiles)
+  
+  for(i in 1:nfiles){
+    out_file[[i]]<-read.table(outNames[i], header = TRUE, skip=2)
+  }
+  return(out_file)
 }
+
+
