9112737c9c8756793dbf90d994435cc1e49bdc04
stanfill <bryan.stanfill@csiro.au>
2014-04-23 15:43:38 +1000

    add modified Morris method function

--- R/sensitivity.R
+++ R/sensitivity.R
@@ -1,4 +1,4 @@
-#' Implement the Morris Method (1991)
+#' Implement the Morris Method According to Morris (1991)
 #' 
 #' This function will generate a random orientation matrix (B*) that is used to 
 #' implement the Morris method.  
@@ -35,6 +35,47 @@ mm1991<-function(p,k){
   
   #xstar is the randomly generated 'base value' of x
   xstar<-matrix(sample(poss,k,replace=T),nrow=1)
+  
+  #Consturct the randomized Bstar matrix
+  
+  p1<-(delta/2)*((2*B-matrix(1,k+1,k))%*%Dstar+matrix(1,k+1,k))
+  Bstar<-(matrix(1,k+1,1)%*%xstar+p1)%*%Pstar
+  return(list(order=toOne,Bstar=Bstar))
+}
+
+#' Implement a modified Morris Method 
+#' 
+#' This function will generate a random orientation matrix (B*) that is used to 
+#' implement the modified Morris method.  This method is modified in that the values
+#' are chosen uniformly on the interval [0,1] and delta is set explicitly by the user
+#' 
+#' @param delta step size
+#' @param k number of parameters
+#' @return  list of 
+#' \itemize{
+#'          \item \code{order} The order in which the variables are changed
+#'          \item \code{Bstar} The random permutation matrix
+#'          }
+#' @examples
+#' mm1991mod(.1,2)
+
+mm1991mod<-function(delta,k){
+  
+  #B is a lower triangular matrix of 1's
+  B<-matrix(0,k+1,k)
+  B[lower.tri(B)]<-1  
+  
+  #Dstar is a k-by-k diagonal matrix with entries +-1 with equal probability
+  Dstar<-diag(sample(c(-1,1),k,replace=T))
+  
+  #Pstar is a random permutation matrix
+  Pstar<-matrix(0,k,k)
+  toOne<-sample(1:k)
+  for(j in 1:k) Pstar[j,toOne[j]]<-1
+  
+  
+  #xstar is the randomly generated 'base value' of x
+  xstar<-matrix(runif(k),nrow=1)
   
   #Consturct the randomized Bstar matrix
   
