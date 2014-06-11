3702ac3f5ec44987807ff105ab7ef459e06d7f6e
stanfill <bryan.stanfill@csiro.au>
2014-06-11 14:19:27 +1000

    use melt and facets to plot all on one ggplot2 object

--- R/visualize.R
+++ R/visualize.R
@@ -4,6 +4,7 @@
 #' @param x Data frame of class 'apsim' including the results of an APSIM simulation
 #' @param y variable to plot on y-axis.  If left empty all variables will be plotted on seperate plots.
 #' @param ask logical; if \code{TRUE}, the user is asked before ach plot, see \code{\link{par}}(ask=.) 
+#' @param one_plot logical; if \code{TRUE} all variables are plotted on one faceted plot
 #' @param ... additional arguments passed to \code{\link[ggplot2:qplot]{qplot}}
 #' @return nothing is returned
 #' @S3method plot apsim
@@ -11,19 +12,22 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' exe <-" \"C:/Program Files (x86)/Apsim76-r3376/Model/Apsim.exe\" "
+#' exe <-"C:/Program Files (x86)/Apsim76-r3376/Model/Apsim.exe"
 #' wd <- "~/APSIM"
-#' toRun <- c("Centro.apsim","Continuous Wheat.apsim")
+#' toRun <- c("Centro.apsim", "Continuous Wheat.apsim")
 #' results <- apsim(exe, wd, files = toRun)
 #' 
-#' #Look at all of the results as a function of time
+#' #Look at all of the results as a function of time in seperate plots
 #' plot(results[[2]])
 #' 
+#' #Put all variables on one faceted plot
+#' plot(results[[2]], one_plot = TRUE) + theme_bw()
+#' 
 #' #Plot just yield as a function of time
-#' plot(results[[2]],y='yield')+geom_line(colour='red')+theme_bw()
+#' plot(results[[2]], y = 'yield') + geom_line(colour = 'red') + theme_bw()
 #' }
 
-plot.apsim<-function(x,y=NULL,ask=TRUE,...){
+plot.apsim<-function(x, y = NULL, ask = TRUE, one_plot = FALSE, ...){
   ncol<-ncol(x)
   cNames<-colnames(x)
   idNum<-which('Date'%in%cNames)
@@ -39,6 +43,13 @@ plot.apsim<-function(x,y=NULL,ask=TRUE,...){
   if(length(y)==1){
 
     return(qplot(x[,'Date'],x[,y],xlab='Date',ylab=y,...))
+    
+  }else if(one_plot==TRUE){
+    
+    mX <- melt(x,id.vars='Date')
+    
+    return(qplot(Date,value,data=mX,...)+facet_wrap(~variable,scales='free'))
+    
     
   }else{
     
