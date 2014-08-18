30420f59301c639b95970ec0f180b81d4c1d0d29
stanfill <bryan.stanfill@csiro.au>
2014-08-18 10:03:12 +1000

    add news file, small edits

--- R/sensitivityAPSIM.R
+++ R/sensitivityAPSIM.R
@@ -21,17 +21,19 @@
 #' 
 #' wd <- "~/APSIM"
 #' var <- c(rep("SoilWater/Thickness",11), "SoilOrganicMatter/SoilCN")
-#' value <- matrix(c(rep(200, 2), rep(300, 9), 10,rep(250, 2), rep(350, 9), 12),nrow=2,byrow=T)
+#' value <- matrix(c(rep(200, 2), rep(300, 9), 10,rep(350, 2), rep(350, 9), 5),nrow=2,byrow=T)
 #' exe <-"C:/Program Files (x86)/Apsim76-r3376/Model/Apsim.exe"
 #' file <- "Canopy.apsim"
 #' 
-#' res <- apsimSEN(X=value,exe=exe, wd=wd,vars=var,toRun=file,g=g)
+#' res <- apsimSEN(X=value,exe=exe, wd=wd,vars=var,toRun=file, toEdit=file,g=g)
+#' res
 #' }
 
 apsimSEN<-function(X, exe, wd, vars, toRun, toEdit=toRun, overwrite=FALSE, g){
   #This is a version of the 'apsim' function that can be 
   #used with the sensitivity package
-  
+  oldWd<-getwd()
+  setwd(wd)
   if(ncol(X)!=length(vars)){
     stop("X must have same number of columns as 'var' is long")
   }
@@ -45,31 +47,32 @@ apsimSEN<-function(X, exe, wd, vars, toRun, toEdit=toRun, overwrite=FALSE, g){
   numVar <- length(unVar)
   valueI<-vector("list",numVar)
   
+  #When overwrite==FALSE then an new .apsim file is created with the title "___-edited.apsim"
+  #Therefore, if toRun==toEdit and overwirte==FALSE then the unchanged version of toEdit is executed
+  #This updates the file name so the correct (new) .apsim file is run
+  if(!overwrite & toEdit==toRun){
+    newName<-paste(gsub(".apsim","",toRun),"-edited",sep="")
+    toRun<-paste(newName,".apsim",sep="")
+  }
+  
   for(i in 1:N){
     
     for(j in 1:numVar){
       valueI[[j]] <- X[i,which(vars==unVar[j])]
     }
     
-    if(grep(".apsim$",toEdit)>0){
+    if(length(grep(".apsim$",toEdit))>0){
       #edit the .apsim file
       edit_apsim(file=toEdit,wd=wd,var=unVar,value=valueI,overwrite=overwrite)
-      
-      #When overwrite==FALSE then an new .apsim file is created with the title "___-edited.apsim"
-      #Therefore, if toRun==toEdit and overwirte==FALSE then the unchanged version of toEdit is executed
-      #This updates the file name so the correct (new) .apsim file is run
-      if(!overwrite & toEdit==toRun){
-        newName<-paste(gsub(".apsim","",file),"-edited",sep="")
-        toRun<-paste(newName,".apsim",sep="")
-      }
-      
     }else{
+      print("sim")
       edit_sim_file(file=toEdit,wd=wd,var=unVar,value=valueI,overwrite=overwrite)
     }
-    
+
     #Run the edited apsim file and collect the output
     res <- apsim(exe=exe, wd=wd, files = toRun)
     y[i] <- g(res)
   }
+  setwd(oldWd)
   return(y)
 }
