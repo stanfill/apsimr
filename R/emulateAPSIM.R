9f8bd005df82f5848334daf5dab195e2261f8da1
stanfill <bryan.stanfill@csiro.au>
2014-12-16 14:43:12 +1000

    update figure, add emulation capability

---
+++ R/emulateAPSIM.R
@@ -0,0 +1,137 @@
+#' A function to emulate complex computer models
+#' 
+#' This is a generic function that can be used to emulate complex computer models
+#' such as APSIM.
+#' 
+#' @param model The model that is of interest
+#' @param X The matrix of inputs at which to evaluate the model
+#' @param method The method to use to emulate the model
+#' @param ... Additional arguments passed to the choosen method
+#' @return A data frame of results, the exact form depends on the method
+#' @export
+
+emulate <- function(model, X, method = "singleGAM",...){
+  
+  method <- try(match.arg(method,c("singleGAM","separateGAM")),silent=TRUE)
+  if(class(method)=='try-error')
+    stop("the only method currently available is 'singleGAM'")
+  
+  if(method=='singleGAM'){
+    res <- singleGAM(model = model, X = X, ...)
+  }else if(method == 'separateGAM'){
+    res <- separateGAM(model = model, X = X, ...)
+  }  
+  
+  return(res)
+}
+
+
+singleGAM <- function(model, X, boot = 1000, conf = 0.95, y = NULL, ...){
+  
+  #This function evaulates the simulator at each row of X,
+  #then uses the simulator outputs to fit only one GAM relating every
+  #first and second order effect of the columns of X to y.  Then for each
+  #input the first order sensitivty index for Xi is estimated by computing
+  #the variance of the fitted values when only Xi and the total
+  #index is estiamted by the variance of the fitted values
+  #when everything except Xi is used.
+  
+  if(is.null(y)){
+    y <- model(X,...)
+  }
+  p <- ncol(X)
+  vY <- var(y)
+  if(is.null(colnames(X))){
+    colnames(X) <- 1:p
+    for(i in 1:p){
+      colnames(X)[i] <- paste0("X",i)
+    }
+  }
+  
+  cnames <- colnames(X)
+  ints <- combn(cnames,2)
+  XallNames <- c(cnames,rep(NA,ncol(ints)))
+  
+  for(j in 1:ncol(ints)){
+    XallNames[(j+p)] <- paste(ints[1,j],ints[2,j],sep=',')
+  }
+  
+  formi<-paste0("y~ti(",XallNames[1])
+  for(j in 2:length(XallNames)){
+    formi <- paste0(formi,")+ti(",XallNames[j])
+  }
+  formi <- formula(paste0(formi,")"))
+  
+  #Fit the GAM and predict from it, i.e. emulate
+  gamFit <- gam(formi,data=data.frame(cbind(y,X)))
+  #vY <- var(predict(gamFit))
+  xStar <- predict(gamFit,type='lpmatrix')
+  bhat <- gamFit$coef
+  V <- gamFit$Vp
+  xstarNames <- colnames(xStar)
+  
+  Sidf <- STdf <- data.frame(X=cnames,Est=0,SE=0,Bias=0,Lower=0,Upper=0)
+  STdf$EstAlt <- 0
+  
+  for(i in 1:p){
+    #Estimate STi
+    xiCols <- grep(cnames[i],xstarNames)
+    xStari <- xStar
+    xStari[,c(xiCols)] <- 0 #Every column that has to do with Xi is made zero
+    ghati <- xStari%*%bhat
+    STdf$Est[i] <- 1-var(ghati)/vY
+    #Try the other way, that is, include all terms involving Xi and use
+    #variance explained by that divided by total variance
+    XstariAlt <- xStar
+    XstariAlt[,-c(xiCols)] <- 0
+    ghatiAlt <- XstariAlt%*%bhat
+    STdf$EstAlt[i] <- var(ghatiAlt)/vY
+    
+    #Bootstrap the bhats to get SE and CIs for total indices
+    sample.coef <- mvrnorm(boot,bhat,V)
+    ghats <- sample.coef%*%t(xStari)
+    BootVis <- apply(ghats,1,var)
+    BootSTis <- 1-BootVis/vY
+    STdf$SE[i] <- sd(BootSTis)
+    STCis <- quantile(BootSTis,c((1-conf)/2,1-(1-conf)/2))
+    STdf$Lower[i] <- STCis[1]
+    STdf$Upper[i] <- STCis[2]
+    STdf$Bias[i] <- mean(BootSTis) - STdf$Est[i]
+    
+    #Estimate Si
+    xiCols <- grep(paste0("(",cnames[i],")"),xstarNames,fixed=TRUE)
+    xStari <- xStar
+    xStari[,-c(1,xiCols)] <- 0
+    ghati <- xStari%*%bhat
+    Sidf$Est[i] <- var(ghati)/vY
+    
+    #Use the bootstrapped bhats to get SE and CIs for first order indices
+    ghats <- sample.coef%*%t(xStari)
+    BootVs <- apply(ghats,1,var)
+    BootSis <- BootVs/vY
+    Sidf$SE[i] <- sd(BootSis)
+    SCis <- quantile(BootSis,c((1-conf)/2,1-(1-conf)/2))
+    Sidf$Lower[i] <- SCis[1]
+    Sidf$Upper[i] <- SCis[2]
+    Sidf$Bias[i] <- mean(BootSis) - Sidf$Est[i]
+  }
+  
+  return(list(FirstOrder=Sidf,Total=STdf,Ehat=gamFit$residuals,Yhat=gamFit$fitted))
+}
+
+separateGAM <- function(model, X, boot = 1000, conf = 0.95, y = NULL,...){
+  
+  if(is.null(y)){
+    y <- model(X,...)
+  }
+  vY <- var(y)
+  p <- ncol(X)
+  Si <- rep(0,p)
+  for(i in 1:p){
+    yhat <- predict(gam(y~s(X[,i])))
+    Si[i] <- var(yhat)/vY
+  }
+  
+  return(Si)
+  
+}
